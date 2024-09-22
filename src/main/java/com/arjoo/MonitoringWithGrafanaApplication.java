0bdb469d94f55cb32d6743a2812f77d500499f7b
arjoo <2009arjoo@gmail.com>
2024-09-22 23:50:45 +0530

    Added initial changes

---
+++ src/main/java/com/arjoo/MonitoringWithGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.arjoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringWithGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringWithGrafanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arjoo/MonitoringWithGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringWithGrafanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
