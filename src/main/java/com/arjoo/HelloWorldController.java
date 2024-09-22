4d5d27e47e3a12277ffc9ca31ff06ad8de13d414
arjoo <2009arjoo@gmail.com>
2024-09-23 00:03:20 +0530

    Added helloWorldController

---
+++ src/main/java/com/arjoo/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.arjoo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloWorld")
+@Slf4j
+public class HelloWorldController {
+
+
+    @GetMapping
+    public String sayHi() {
+        log.info("logging hello world message");
+        return "Hello world";
+    }
+}
